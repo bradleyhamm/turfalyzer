ae1c91bfc9d77965fb483368c839ccc218c6d704
Bradley Hamm <bradjh83@gmail.com>
2014-12-31 23:59:05 -0800

    Nearly completed web worker to get RF for combos, sort, slice, and return to main page

--- js/turf.js
+++ js/turf.js
@@ -28,7 +28,7 @@ Turf.prototype.replaceData = function (data) {
  * @param {Number}  The 0-based index of this data record
  * @param {Array}   Array of k 0-based item indexes in this combination
  */
-Turf.prototype.getRecordFrequency = function (recordIndex, combo) {
+Turf.prototype._getRecordFrequency = function (recordIndex, combo) {
 
     // Select this record's data
     var data = this.data[recordIndex];
@@ -38,7 +38,7 @@ Turf.prototype.getRecordFrequency = function (recordIndex, combo) {
         // Get the corresponding boolean values from this record's data
         .map(function (itemIndex) {
             return data[itemIndex];
-        })
+        });
 
         // Count the number of hits
         .reduce(function (previousValue, currentValue) {
@@ -47,12 +47,57 @@ Turf.prototype.getRecordFrequency = function (recordIndex, combo) {
 
 };
 
+Turf.prototype._sortResults = function (a, b) {
+    return (a.reach - b.reach);
+};
+
+/**
+ * @param {Array}   Array of k 0-based item indexes in this combination
+ */
+Turf.prototype._doComboTurf = function (combo) {
+    var reach = 0;
+    var frequency = 0;
+
+    var recordFreq;
+
+    for (var ri = 0; ri < this.data.length; ri++) {
+        recordFreq = this._getRecordFrequency(ri, combo);
+        reach += Number(Boolean(recordFreq));
+        frequency += recordFreq;
+    }
+
+    this.results.push({
+        combo: combo,
+        reach: reach,
+        frequency: frequency
+    });
+
+    this.results.sort(this._sortResults).slice(0, this.count);
+};
+
+Turf.prototype._checkStop() {
+    if (this.stop) {
+        return true;
+    }
+}
+
 /**
  * @param {Number}  The number of items desired per combination
  * @param {Number}  The number of top combinations to return
  */
 Turf.prototype.getResults = function (k, count) {
+    var combo;
+    var comboResults;
+
     this.results = [];
+
+    if (this.k === undefined) {
+        throw new ReferenceError("Combination size not set");
+    }
+
+    if (this.count === undefined) {
+        throw new ReferenceError("Number of results not set")
+    }
 
     if (this.data === undefined) {
         throw new ReferenceError("No data available");
@@ -60,24 +105,57 @@ Turf.prototype.getResults = function (k, count) {
 
     this.combination_gen.k = k;
     this.combination_gen.build();
+
+    this._doComboTurf(this.combination_gen.get_combination());
+    if (this._checkStop()) {
+        return null;
+    }
+
+    while (this.combination_gen.next()) {
+        if (this._checkStop()) {
+            return null;
+        }
+        this._doComboTurf(this.combination_gen.get_combination());
+    }
+
+    return this.results;
+
 };
 
 var turf = new Turf();
 
 self.addEventListener("message", function (e) {
-    var stop = e.data.stop || false;
     var k = e.data.k;
     var data = e.data.data;
     var count = e.data.count;
     var results;
 
+    this.stop = e.data.stop || false;
+
     if (data !== undefined) {
         turf.replaceData(data);
     }
 
-    if (k !== undefined && count !== undefined) {
-        self.postMessage({
-            results: turf.getResults(k, count)
-        });
+    if (k !== undefined) {
+        this.k = k;
     }
+
+    if (count !== undefined) {
+        this.count = count;
+    }
+
+    // Run analysis
+    results = turf.getResults();
+
+    // Analysis was interrupted
+    if (results === null) {
+        self.postMessage({
+            stopped: true
+        })
+    }
+
+    // Send results to main page
+    self.postMessage({
+        results: results
+    });
 });
